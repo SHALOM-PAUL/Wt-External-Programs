0abbf85e631039e409ed967341a24953f63bcab8
SHALOM-PAUL <shalompaul.missionary@gmail.com>
2017-04-25 19:02:21 +0530

    new site wt programs

---
+++ _site/php/login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+	<?php
+		echo "you have been succesfully logged in";
+	?>
+</body>
+</html>
---
+++ php/register.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+	<?php
+		echo "you have been Registered";
+	?>
+</body>
+</html>
